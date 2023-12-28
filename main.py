cb41bd940a5d485bb65cad839b5c3e0d59bf3c14
kiwik-cik <145658906+kiwik-cik@users.noreply.github.com>
2023-12-28 10:11:26 +0300

    main.py

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-class Transport:
-    def __init__(self, brand, max_speed, kind=None):
-        self.brand = brand
-        self.max_speed = max_speed
-        self.kind = kind
-    def __str__(self):
+class Transport: # Объявление класса Transport
+    def __init__(self, brand, max_speed, kind=None):  # Определение метода инициализации
+        self.brand = brand  # Установка марки транспорта
+        self.max_speed = max_speed  # Установка максимальной скорости
+        self.kind = kind  # Установка типа транспорта
+    def __str__(self):  # Определение метода для преобразования в строку
         return f'Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч'
-class Car(Transport):
-    def __init__(self, brand, max_speed, mileage, gasoline_residue):
-        super().__init__(brand,max_speed,kind='Car')
-        self.mileage = mileage
-        self.__gasoline_residue = gasoline_residue
 
-    @property
-    def gasoline(self):
+class Car(Transport):  # Объявление класса Car, который наследуется от класса Transport
+    def __init__(self, brand, max_speed, mileage, gasoline_residue):  # Определение метода инициализации
+        super().__init__(brand, max_speed, kind='Car')  # Вызов метода инициализации родительского класса
+        self.mileage = mileage  # Установка пробега
+        self.__gasoline_residue = gasoline_residue  # Установка остатка бензина (приватное свойство)
+
+    @property  # Определение декоратора для создания свойства gasoline
+    def gasoline(self):  # Геттер для свойства gasoline
         return f'Осталось бензина на {self.__gasoline_residue} км'
-    @gasoline.setter
-    def gasoline(self, value):
-        if isinstance(value,int):
-            self.__gasoline_residue += value
+
+    @gasoline.setter  # Определение сеттера для свойства gasoline
+    def gasoline(self, value):  # Сеттер для свойства gasoline
+        if isinstance(value, int):  # Проверка, что значение является целым числом
+            self.__gasoline_residue += value  # Увеличение остатка бензина на заданное значение
             print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
         else:
-            print('Ошибка заправки автомобиля')
-class Boat(Transport):
-    def __init__(self, brand, max_speed, owners_name):
-        super().__init__(brand, max_speed, kind='Boat')
-        self.owners_name = owners_name
-    def __str__(self):
-        return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
-class Plane(Transport):
-    def __init__(self, brand, max_speed, capacity):
-        super().__init__(brand, max_speed, kind='Plane')
-        self.capacity = capacity
-    def __str__(self):
-        return f'Самолет марки {self.brand} вмещает в себя {self.capacity} людей'
-car = Car('Audi', 250, 10000, 50)
-boat = Boat('Yamaha', 80, 'John')
-plane = Plane('Boeing', 900, 300)
+            print('Ошибка заправки автомобиля')  # Вывод сообщения об ошибке заправки автомобиля
 
-print(car)
-print(boat)
-print(plane)
+class Boat(Transport):  # Объявление класса Boat, который наследуется от класса Transport
+    def __init__(self, brand, max_speed, owners_name):  # Определение метода инициализации
+        super().__init__(brand, max_speed, kind='Boat')  # Вызов метода инициализации родительского класса
+        self.owners_name = owners_name  # Установка имени владельца
+
+    def __str__(self):  # Определение метода для преобразования в строку
+        return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
+
+class Plane(Transport):  # Объявление класса Plane, который наследуется от класса Transport
+    def __init__(self, brand, max_speed, capacity):  # Определение метода инициализации
+        super().__init__(brand, max_speed, kind='Plane')  # Вызов метода инициализации родительского класса
+        self.capacity = capacity  # Установка вместимости
+
+    def __str__(self):  # Определение метода для преобразования в строку
+        return f'Самолет марки {self.brand} вмещает в себя {self.capacity} людей'
+
+car = Car('Audi', 250, 10000, 50)  # Создание экземпляра класса Car
+boat = Boat('Boeing', 350, 'John')  # Создание экземпляра класса Boat
+plane = Plane('Boeing', 900, 300) # Создание экземпляра класса Plane
+
+print(car) # Выводим Car
+print(boat) # Выводим Boat
+print(plane) # Выводим Plane
